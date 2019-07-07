56cbc5ff9c1e69d2ee1d0d7f8b3afed01ebbe30d
code325 <47559770+code325@users.noreply.github.com>
2019-07-07 21:30:58 +0900

    all new.

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('#sidebarCollapse').on('click', function () {
-    $('#sidebar').addClass('active');
-    $('.overlay').fadeIn();
-});
-
-$('.overlay').on('click', function () {
-    $('#sidebar').removeClass('active');
-    $('.overlay').fadeOut();
-});
